3a83d705bd32aace12ef8294aa5cccde4c4bfc23
posinia <posinia@naver.com>
2019-01-03 16:25:52 +0900

    video

---
+++ src/components/search_bar.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+// 상단에 입력창
+// 타이핑 가능
+// 비디오 리스트 업데이트
+// 유투브 API 요청
+/*
+const SearchBar = () => {
+	return <input />; //React.createElement
+};
+
+*/
+
+class SearchBar extends Component {
+	constructor(props) {
+		super(props); // 부모 메소드 호출
+		console.log(props);
+		this.state = {term: ''}; // state정의
+		// term: 사용자가 검색 인풋에 업데이트 할 때마다 term이 업데이트 되거나 변경사항을 받아옴
+	}
+
+	render() {
+		//return <input onChange={this.onInputChange}/>;
+		// return <input onChange={ event => console.log(event.target.value)} />;
+		// this.state.term = event.target.value // BAD!
+		console.log('search bar render: ' + this.state.term);
+
+		return (
+			<div className="search-bar">
+				<input
+					value={this.state.term}
+					onChange={ event => this.onInputChange(event.target.value)} />
+				 {/*<div>Value of the input: {this.state.term}</div>*/}
+			</div>
+		);
+	}
+
+	onInputChange(term) {
+		//onSearchTermChange
+		this.setState({ term: term });
+		this.props.onSearchTermChange(term);
+	}
+
+	/*
+	onInputChange(event) { // 함수명 handleInputChange 라고도 만이씀 (이벤트 핸들러)
+		console.log('change', event.target.value);
+		console.log(event);
+	}
+	*/
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React, { Component } from 'react'; // node_modules ..
+import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyAh5OrIJlagQPBDgeWY9JMvLJB85GFkq-s';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+//Create a new Component. This component should produce
+//some HTML
+class App extends Component {
+	constructor(props) {
+		super(props);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+
+		this.videoSearch('bigbang');
+	}
+
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, (videos) => {
+			console.log(videos);
+
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0],
+			}); // key와 변수 이름이 같을때 this.setState({ videos: videos})
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+				<VideoDetail video={this.state.selectedVideo}/>
+				<VideoList
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+					videos={this.state.videos}/>
+			</div>
+		);
+	}
+}
+
+//Take this component's generated HTML and and put it
+//on the page (in the DOM)
+ReactDom.render(<App/>, document.querySelector('.container-fluid'));
